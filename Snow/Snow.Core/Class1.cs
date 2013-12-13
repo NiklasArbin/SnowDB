43de9680fe90a4e02fbfee891abab092eb59d335
niklasarbin <niklas.arbin@ffcg.se>
2013-12-14 00:12:08 +0100

    basic interface

--- Snow/Snow.Core/Class1.cs
+++ Snow/Snow.Core/Class1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +11,43 @@ namespace Snow.Core
 {
     public class DocumentDocumentStore : IDocumentStore
     {
+        private DirectoryInfo _dataDirectory;
+
         public void Initialize()
+        {
+            _dataDirectory = new DirectoryInfo(DataLocation);
+            if(!_dataDirectory.Exists)
+                throw new DirectoryNotFoundException(String.Format("The DataLocation '{0}' doesn't exist", DataLocation));
+            _dataDirectory.MoveTo(Name);
+            if(!_dataDirectory.Exists)
+                _dataDirectory.Create();
+        }
+
+        public IDocumentSession OpenSession()
         {
             throw new NotImplementedException();
         }
+
+        public string Name { get; set; }
+        public string DataLocation { get; set; }
     }
 
     public interface IDocumentStore
     {
         void Initialize();
+        IDocumentSession OpenSession(); 
+        string Name { get; set; }
+        string DataLocation { get; set; }
     }
 
     public class DocumentSession : IDocumentSession
     {
-        public void Store(object document)
+        public void Get<TDocument>(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store<TDocument>(TDocument document)
         {
             throw new NotImplementedException();
         }
@@ -34,7 +60,8 @@ namespace Snow.Core
 
     public interface IDocumentSession
     {
-        void Store(object document);
+        void Get<TDocument>(object key);
+        void Store<TDocument>(TDocument document);
         void SaveChanges();
     }
 }
