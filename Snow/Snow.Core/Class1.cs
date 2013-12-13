321350777df962d38e3f6da79ca8857905992da9
niklas.arbin@ffcg.se <niklas.arbin@ffcg.se>
2013-12-13 23:21:48 +0100

    add serializer and base interface

--- Snow/Snow.Core/Class1.cs
+++ Snow/Snow.Core/Class1.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Snow.Core
 {
-    public class Class1
+    public class DocumentDocumentStore : IDocumentStore
     {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IDocumentStore
+    {
+        void Initialize();
+    }
+
+    public class DocumentSession : IDocumentSession
+    {
+        public void Store(object document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IDocumentSession
+    {
+        void Store(object document);
+        void SaveChanges();
     }
 }
