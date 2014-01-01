50f8cd0b7cfce8142d260ecee431328ddc903788
niklas.arbin@ffcg.se <niklas.arbin@ffcg.se>
2014-01-02 00:52:06 +0100

    add FastJson Serializer

---
+++ Snow/Snow.Core/Serializers/FastJsonSerializer.cs
@@ -0,0 +1,15 @@
+﻿namespace Snow.Core.Serializers
+{
+    public class FastJsonSerializer : IDocumentSerializer
+    {
+        public string Serialize<TDocument>(TDocument value)
+        {
+            return fastJSON.JSON.Instance.ToJSON(value);
+        }
+
+        public TDocument Deserialize<TDocument>(string serialized)
+        {
+            return fastJSON.JSON.Instance.ToObject<TDocument>(serialized);
+        }
+    }
+}
