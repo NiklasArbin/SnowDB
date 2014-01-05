e31facf020f62d076ffd024efdbcf680519634a6
niklas.arbin@ffcg.se <niklas.arbin@ffcg.se>
2014-01-06 00:03:08 +0100

    add snow indexing

--- Snow/Snow.Core/Lucene/Index.cs
+++ Snow/Snow.Core/Lucene/Index.cs
@@ -1,28 +1,82 @@
-﻿//using System;
-//using System.Data.SqlTypes;
-//using System.Linq;
-//using System.Reflection;
-//using Lucene.Net.Documents;
+﻿using System;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Snow.Core.Extensions;
+using Snow.Core.Operation;
+using Version = Lucene.Net.Util.Version;
 
-//namespace Snow.Core.Lucene
-//{
-//    internal class Index
-//    {
-//        public void Add<TDocument>(TDocument document)
-//        {
-//            var luceneDoc = new Document();
-//            luceneDoc.Add(new Field());
-//        }
-//        public void Delete<TDocument>(TDocument document)
-//        { }
+namespace Snow.Core.Lucene
+{
+    internal interface ISnowIndex : IDisposable, ISnowTransaction
+    {
+        void Add<TDocument>(string key, string json);
+        void Delete<TDocument>(string key);
+    }
 
-//        private void GetPublicProperties(Type type, object o)
-//        {
-//            var properties = type.GetProperties(BindingFlags.Public);
-//            foreach (var propertyInfo in properties)
-//            {
-//                o.
-//            }
-//        }
-//    }
-//}
+    internal class SnowIndex : ISnowIndex
+    {
+        private const string SnowDbKeyName = "SnowDBKey";
+
+        private readonly IndexWriter _writer;
+        private readonly FSDirectory _fsDirectory;
+        private readonly Analyzer _analyser = new StandardAnalyzer(Version.LUCENE_30);
+
+        private SnowIndex(IDocumentFileNameProvider fileNameProvider)
+        {
+            _fsDirectory = FSDirectory.Open(fileNameProvider.GetLuceneDirectory().FullName);
+            _writer = new IndexWriter(_fsDirectory, _analyser, IndexWriter.MaxFieldLength.UNLIMITED);
+        }
+
+        public static SnowIndex Open(IDocumentFileNameProvider fileNameProvider)
+        {
+            return new SnowIndex(fileNameProvider);
+        }
+
+        public void Add<TDocument>(string key, string json)
+        {
+            var doc = new Document();
+            doc.Add(new Field(SnowDbKeyName, GetKey<TDocument>(key), Field.Store.YES, Field.Index.ANALYZED));
+            var fields = LuceneSerializer.Serialize(json);
+            foreach (var field in fields)
+            {
+                doc.Add(field);
+            }
+            _writer.AddDocument(doc);
+        }
+
+        public void Delete<TDocument>(string key)
+        {
+            var term = new Term(SnowDbKeyName, GetKey<TDocument>(key));
+            _writer.DeleteDocuments(term);
+        }
+
+        public void Prepare()
+        {
+            _writer.PrepareCommit();
+        }
+
+        public void Commit()
+        {
+            _writer.Commit();
+        }
+
+        public void Rollback()
+        {
+            _writer.Rollback();
+        }
+
+        public void Dispose()
+        {
+            _writer.Dispose();
+            _fsDirectory.Dispose();
+        }
+
+        private static string GetKey<TDocument>(string key)
+        {
+            return "{0}.{1}".FormatWith(typeof(TDocument).Name, key);
+        }
+    }
+}
---
+++ Snow/Snow.Core/Operation/ISnowTransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snow.Core.Operation
+{
+    internal interface ISnowTransaction
+    {
+        void Prepare();
+        void Commit();
+        void Rollback();
+    }
+}
