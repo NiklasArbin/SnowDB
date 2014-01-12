d3999bbd0834e1f185db750eba9f0556078fdbb3
niklas.arbin@ffcg.se <niklas.arbin@ffcg.se>
2014-01-12 14:25:53 +0100

    mode exception

---
+++ Snow/Snow.Core/DocumentFileTimeoutException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Snow.Core
+{
+    public class DocumentFileTimeoutException : Exception
+    {
+        public DocumentFileTimeoutException(string message)
+            : base(message)
+        { }
+    }
+}
